2dcc3c10ee607986dc0363eee034553777a71cdd
huh <rhdfl7@gmail.com>
2021-05-27 17:37:41 +0900

    Third Commit

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask , render_template
 app = Flask(__name__)
+#app.debug = True
 
-@app.route('/')           #'/'는 정해준 포트값의 정의이다. ('/', methods=['GET','POST'])
-def hello_world():        # app.run(port=$$,debug = true/false)
-    return 'Hello World!' # 포트값 = 주소, 디버그 = 활동내역 true시 해킹 위험
-# @는 '다음 행동과 연결하게 해준다.' method = login, 다음줄 check loging 정도로, 로그인했는지 검사하고 다음 으로 넘겨준다는 뜻
+@app.route('/', methods=['GET', 'POST']) 
+def hello_world():
+    return render_template('home.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
